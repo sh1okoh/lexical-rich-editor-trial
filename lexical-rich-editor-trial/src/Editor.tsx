1216fb2b90c1622cb30eae36e6f7a083c168300a
sh1okoh <kooooo.0719task@gmail.com>
2022-11-02 08:22:21 +0900

    feat: heading nodeの適用

--- lexical-rich-editor-trial/src/Editor.tsx
+++ lexical-rich-editor-trial/src/Editor.tsx
@@ -4,13 +4,16 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import styles from "./Editor.module.scss";
 import { AutoFocusPlugin } from "./plugins/AutoFocusPlugin";
+import { ToolbarPlugin } from "./plugins/ToolbarPlugin";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { nodes } from "./nodes";
 
 
 
 const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
   namespace: "MyEditor",
   onError: (error) => console.error(error),
+  nodes: nodes
 };
 
 const ErrorBoundaryComponent: FC = () => {
@@ -25,8 +28,10 @@ export const Editor: FC = () => {
   return (
     <LexicalComposer initialConfig={initialConfig}>
       <div className={styles.editorContainer}>
+        <ToolbarPlugin />
+
         <RichTextPlugin
-          contentEditable={<ContentEditable/>}
+          contentEditable={<ContentEditable />}
           placeholder={<div>いまなにしてる？</div>}
           ErrorBoundary={ErrorBoundaryComponent}
         />
---
+++ lexical-rich-editor-trial/src/plugins/ToolbarPlugin.tsx
@@ -0,0 +1,74 @@
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { FC, useCallback, useState } from "react";
+import styles from "./ToolbarPlugin.module.scss";
+import { HeadingTagType, $createHeadingNode, } from "@lexical/rich-text";
+import { $getSelection, $isRangeSelection } from "lexical"
+import { $wrapNodes } from "@lexical/selection";
+
+
+
+const SupportedBlockType = {
+  paragraph: "Paragraph",
+  h1: "Heading 1",
+  h2: "Heading 2",
+  h3: "Heading 3",
+  h4: "Heading 4",
+  h5: "Heading 5",
+  h6: "Heading 6",
+} as const;
+
+type BlockType = keyof typeof SupportedBlockType;
+
+export const ToolbarPlugin: FC = () => {
+  const [blockType, setBlockType] = useState<BlockType>("paragraph");
+  const [editor] = useLexicalComposerContext();
+
+  const formatHeading = useCallback(
+    (type: HeadingTagType) => {
+      if (blockType !== type) {
+        editor.update(() => {
+          const selection = $getSelection();
+          if ($isRangeSelection(selection)) {
+            console.log('selection', selection);
+            $wrapNodes(selection, () => $createHeadingNode(type));
+          }
+        });
+      }
+    },
+    [blockType, editor],
+  );
+  return (
+    <div className={styles.toolbar}>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType["h1"]}
+        aria-label={SupportedBlockType["h1"]}
+        aria-checked={blockType === "h1"}
+        onClick={() => formatHeading('h1')}
+      >
+        h1
+      </button>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType["h2"]}
+        aria-label={SupportedBlockType["h2"]}
+        aria-checked={blockType === "h2"}
+        onClick={() => formatHeading('h2')}
+      >
+        h2
+      </button>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType["h3"]}
+        aria-label={SupportedBlockType["h3"]}
+        aria-checked={blockType === "h3"}
+        onClick={() => formatHeading('h3')}
+      >
+        h3
+      </button>
+    </div>
+  );
+};
