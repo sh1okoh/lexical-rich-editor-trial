885029fd7445735cc7d54bf999ffa53f40731a4a
sh1okoh <kooooo.0719task@gmail.com>
2022-11-08 01:35:26 +0900

    feat: editorStateをserializeしたりdeserializeしたり

--- lexical-rich-editor-trial-2/src/plugins/ConvertEditorStatePlugin.tsx
+++ lexical-rich-editor-trial-2/src/plugins/ConvertEditorStatePlugin.tsx
@@ -1,18 +1,32 @@
 import { FC } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { SerializedEditorState } from "lexical";
 
 
 export const ConvertEditorState: FC = () => {
   const [editor] = useLexicalComposerContext();
-  const handleOnClick = () => {
+  let globalJson: SerializedEditorState;
+  const exportJson = () => {
     const editorState = editor.getEditorState();
     const json = editorState.toJSON();
+    globalJson = json;
     console.log('json', json);
+  }
+
+  const importJson = () => {
+    editor.update(() => {
+      const editorState = editor.parseEditorState(globalJson);
+      console.log('editorState', editorState);
+      editor.setEditorState(editorState);
+    })
   }
   return (
     <>
-    <button onClick={handleOnClick}>
-      送信するよ！
+    <button onClick={exportJson}>
+      exportJson
+    </button>
+    <button onClick={importJson}>
+      importJson
     </button>
     </>
   );
