30153041204a3b501fe03231c215a62ea13f6913
sh1okoh <kooooo.0719task@gmail.com>
2022-11-11 10:02:27 +0900

    add

---
+++ lexical-rich-editor-trial-2/src/plugins/BackgroundImagePlugin.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+
+export const BackgroundImagePlugin: FC = () => {
+  const [editor] = useLexicalComposerContext();
+  return (
+    <>
+    hoge
+    </>
+  );
+};
