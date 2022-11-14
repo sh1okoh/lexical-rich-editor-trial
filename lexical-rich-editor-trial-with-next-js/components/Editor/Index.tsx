a1f89340437eb9b8019d096772eac99d41423555
sh1okoh <kooooo.0719task@gmail.com>
2022-11-14 23:28:00 +0900

    feat: scss

--- lexical-rich-editor-trial-with-next-js/components/Editor/Index.tsx
+++ lexical-rich-editor-trial-with-next-js/components/Editor/Index.tsx
@@ -3,20 +3,23 @@ import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import styles from "./Editor.module.scss";
 
 const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
   namespace: "MyEditor",
   onError: (error) => console.error(error),
 };
 
-const Index: FC = () => {
+export const Index: FC = () => {
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>いまなにしてる？</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
+      <div className={styles.editorContainer}>
+        <RichTextPlugin
+          contentEditable={<ContentEditable className={styles.contentEditable} />}
+          placeholder={<div className={styles.placeholder}>いまなにしてる？</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </div>
     </LexicalComposer>
   );
 };
